c0a8f0e924ca378c37365c766f346bc420bbaad4
Juan Antonio Mosquera <juanantoniomosquera@juanantoniomosquera.com>
2018-02-11 18:59:24 +0100

    correcion objetos

--- group/near.php
+++ group/near.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
  
 // include database and object files
 include_once '../config/database.php';
-include_once '../groups/group.php';
+include_once '../objects/group.php';
  
 // instantiate database and group object
 $database = new Database();
