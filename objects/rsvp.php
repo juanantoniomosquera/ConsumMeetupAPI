c000a4b68fc2ed6b8f26d7c56932d11d73c52948
Juan Antonio Mosquera <juanantoniomosquera@juanantoniomosquera.com>
2018-02-13 12:50:13 +0100

    correcion parametros en consulta

--- objects/rsvp.php
+++ objects/rsvp.php
@@ -64,7 +64,7 @@ class Rsvp {
                 " . $this->tableName . "
             SET
                 group_id=:group_id, group_name=:group_name, group_city=:group_city, group_country=:group_country, group_lon=:group_lon, group_lat=:group_lat
-                rsvp_id=:rsvp_id,event_id=:event_id,event_name=:event_name,event_time=:event_time";
+                rsvp_id=:rsvp_id,event_id=:event_id,event_name=:event_name,event_time=:event_time,event_url=:event_url";
  
     $stmt = $this->conn->prepare($query);
  
