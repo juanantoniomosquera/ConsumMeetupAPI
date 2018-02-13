ce7589a66d41a5fb0b228543a8e01d49dd93de14
Juan Antonio Mosquera <juanantoniomosquera@juanantoniomosquera.com>
2018-02-13 10:09:02 +0100

    pruebas

--- group/near.php
+++ group/near.php
@@ -15,6 +15,9 @@
  
   $group = new Group($db);
 
+  print_r($_GET['group_lon']);
+
+
   $group->group_lon = !empty($_GET['group lon']) ? $_GET['group_lon'] : die('Sin valor para longitud');
   $group->group_lat = !empty($_GET['group_lat']) ? $_GET['group_lat'] : die('Sin valor para latitud');
   
